89778e2e3c8cd6da4b90021f3c5a7b8d07652bc8
FedeHorus <110682482+FedeHorus@users.noreply.github.com>
2025-01-23 12:01:02 -0300

    Add files via upload
    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function linkedin(){
+    let linkedin = document.getElementById(LinkedIn)
+}
+
+function añadirTextoBotones(){
+/*************  ✨ Codeium Command 🌟  *************/
+     document.getElementById("LinkedIn").innerText = "LinkedIn";
+     /******  c3200471-b77d-4bd6-abc1-9fd34d73ec7e  *******/
+    
+}
+
+añadirTextoBotones();
+let text = "Soy una persona con mucha creatividad que disfruta de emprender desafíos nuevos que pongan a prueba mis conocimientos. Me apasiona el diseño gráfico y la creación de contenido en redes sociales.En mis años de experiencia trabajando en distintos rubros he desarrollado una gran habilidad para descifrar los gustos y deseos de los usuarios logrando los objetivos planteados. A la hora de trabajar, prefiero los equipos ya que son de gran ayuda para el crecimiento de cada uno de los integrantes por el aporte personal que cada quien puede brindar desde sus experiencias. Y así nutrirse entre todos. Mi otra pasión es la escritura. Publiqué dos libros en los últimos 4 años. Busco unirme a equipos de trabajo donde pueda aportar mis conocimientos y seguir formándome profesionalmente.";
+
+document.getElementById("presenta").innerText = text;
